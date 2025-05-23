362be805ea6c10fc3b62e78e2f5e66da45d51749
Nikita Savvin <nik.savvin.2004@mail.ru>
2025-05-23 20:17:11 +0300

    Init

---
+++ src/main/java/com/ortin/weblinux/WebLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.ortin.weblinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLinuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ortin/weblinux/WebLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortin.weblinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLinuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
